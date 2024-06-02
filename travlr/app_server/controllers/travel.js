11e213d20160d3cc51aa7c641cbedaf9f52c6910
Luis Sanchez <luisanchezdh@gmail.com>
2024-06-02 19:21:52 -0400

    module 3 finished

--- travlr/app_server/controllers/travel.js
+++ travlr/app_server/controllers/travel.js
@@ -1,9 +1,11 @@
-const packageJson = require('../../package.json');
+const fs = require('fs');
+const packageJson = JSON.parse(fs.readFileSync('package.json', 'utf8'));
+const trips = JSON.parse(fs.readFileSync('app_server/data/trips.json', 'utf8'));
 
 /* GET travel view. */
 const travel = (req, res) => {
     pageTitle = packageJson.description + ' | Travel';
-    res.render('travel', { title: pageTitle });
+    res.render('travel', { title: pageTitle, trips });
 };
 
 module.exports = {
